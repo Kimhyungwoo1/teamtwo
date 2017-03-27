557b105743a64bef8ad43985242b48f46ce9d6a5
Dojun <dojun.kim.1@gmail.com>
2017-03-27 17:36:23 +0900

    opensourceServlet

--- SourceMeister/src/main/java/com/meister/opensource/web/OpenSourceViewListServlet.java
+++ SourceMeister/src/main/java/com/meister/opensource/web/OpenSourceViewListServlet.java
@@ -3,9 +3,14 @@ package com.meister.opensource.web;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -16,83 +21,77 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-
+import com.google.gson.Gson;
 
 public class OpenSourceViewListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
-    public OpenSourceViewListServlet() {
-    }
-
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public OpenSourceViewListServlet() {
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/opensource/list.jsp");
+		dispatcher.forward(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String search = request.getParameter("search");
-		
+
 		System.out.println(search);
-		
-		
-		StringBuilder urlBuilder = new StringBuilder("https://searchcode.com/api/codesearch_I/"); 
-        urlBuilder.append("?" + URLEncoder.encode("q","UTF-8") + "=" + search + "+ext:md"); 
-        
-        System.out.println(urlBuilder.toString());
-  
-        URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
-        
-        System.out.println("Response code: " + conn.getResponseCode());
-        
-        BufferedReader rd;
-        
-        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
-        } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream(),"UTF-8"));
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
-        
-        System.out.println(sb.toString());
-        rd.close();
-        conn.disconnect();
+
+		StringBuilder urlBuilder = new StringBuilder("https://searchcode.com/api/codesearch_I/");
+		urlBuilder.append("?" + URLEncoder.encode("q", "UTF-8") + "=" + search + "+ext:md");
+
+		System.out.println(urlBuilder.toString());
+
+		URL url = new URL(urlBuilder.toString());
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Content-type", "application/json");
+
+		System.out.println("Response code: " + conn.getResponseCode());
+
+		BufferedReader rd;
+
+		if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+			rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+		} else {
+			rd = new BufferedReader(new InputStreamReader(conn.getErrorStream(), "UTF-8"));
+		}
+
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = rd.readLine()) != null) {
+			sb.append(line);
+		}
+
+		System.out.println(sb.toString());
+		rd.close();
+		conn.disconnect();
 
 		JSONObject object = new JSONObject(sb.toString());
+		List<String> listApi = new ArrayList<String>();
 
-		
-		
-		
-		
-		
-		JSONArray arr = object.getJSONArray("results"); //배열단위로 추출하고 싶을때
-		String total = object.get("total").toString(); //Object로 추출하고 싶을때
-				
-        StringBuffer list = new StringBuffer();
-		
-		
-		for(int i = 0; i < arr.length(); i ++ ) 
-			 list.append(arr.getJSONObject(i).toString() + "\n\n");
-        
-        request.setAttribute("list", list);
-        request.setAttribute("count", total);
-        
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/list.jsp");
-        dispatcher.forward(request, response);
-        
-    
-        	
-	
+		JSONArray arr = object.getJSONArray("results"); // 배열단위로 추출하고 싶을때
+		String total = object.get("total").toString(); // Object로 추출하고 싶을때
 
+		StringBuffer list = new StringBuffer();
+
+		for (int i = 0; i < arr.length(); i++) {
+			
+			System.out.println(arr.getJSONObject(i).toString() + "\n\n");
+			
+			listApi.add(arr.getJSONObject(i).toString() + "\n\n");
+		}
+		request.setAttribute("count", total);
+		request.setAttribute("sourceList", list);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/opensource/list.jsp");
+		dispatcher.forward(request, response);
 
 	}
 
---
+++ SourceMeister/src/main/webapp/WEB-INF/view/opensource/index.jsp
@@ -0,0 +1,45 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script type="text/javascript" src="/SourceMeister/static/js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript">
+
+	$().ready(function () {
+			
+		$("#search").keydown(function(e) {
+			if (e.which == 13) {
+				submitForm();
+			}
+		});
+			
+		
+		$("#searchForm").find("input[type=button]").click(function() {
+			submitForm();
+		});
+		
+		
+	});
+
+	function submitForm() {
+		$("#searchForm").attr({
+			"action" : "/SourceMeister/opensource",
+			"method" : "post"
+		}).submit();
+	}
+
+</script>
+
+<title>검색을 해보자</title>
+</head>
+<body>
+	
+	<form id = "searchForm">	
+		<input type="text" name="search" id="search"/>
+		<input type="button" value="검색"/>
+	</form>
+	
+</body>
+</html>
