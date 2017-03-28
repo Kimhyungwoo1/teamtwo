7876e4c15f794ff1f408d670049d62e8b09b62ea
Kimhyungwoo1 <woo6145@naver.com>
2017-03-28 16:16:09 +0900

    Update AuthorizationVO.java

--- SourceMeister/src/main/java/com/meister/authorization/vo/AuthorizationVO.java
+++ SourceMeister/src/main/java/com/meister/authorization/vo/AuthorizationVO.java
@@ -2,7 +2,6 @@ package com.meister.authorization.vo;
 
 public class AuthorizationVO {
 
-<<<<<<< HEAD
 	private String AuthorizationId;
 	private String AuthorizationName;
 
@@ -22,25 +21,4 @@ public class AuthorizationVO {
 		AuthorizationName = authorizationName;
 	}
 	
-=======
-	private String authorizationId;
-	private String authorizationName;
-
-	public String getAuthorizationId() {
-		return authorizationId;
-	}
-
-	public void setAuthorizationId(String authorizationId) {
-		this.authorizationId = authorizationId;
-	}
-
-	public String getAuthorizationName() {
-		return authorizationName;
-	}
-
-	public void setAuthorizationName(String authorizationName) {
-		this.authorizationName = authorizationName;
-	}
-
->>>>>>> jun
 }
