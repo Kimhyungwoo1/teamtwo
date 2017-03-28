021f320be80a27c1d68b2f9d4dc00ce1ca7f0c3f
Lizzy <lizzysoo0000@gmail.com>
2017-03-28 14:57:11 +0900

    readme 보여주기 완성

--- SourceMeister/src/main/java/com/meister/opensource/web/ViewProjectDetailServlet.java
+++ SourceMeister/src/main/java/com/meister/opensource/web/ViewProjectDetailServlet.java
@@ -5,9 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -15,10 +12,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
-
-import com.meister.opensource.vo.OpensourceVO;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class ViewProjectDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -34,22 +32,22 @@ public class ViewProjectDetailServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String opensourceId = request.getParameter("opensourceId");
+		
 		/*
-		 * https://searchcode.com/file/101589460/ 오픈 소스 id로 검색되게 만들기
+		 * 프로젝트 내용 
 		 */
 		
-		StringBuilder urlBuilder = new StringBuilder("https://searchcode.com/");
-		urlBuilder.append(URLEncoder.encode("file", "UTF-8") + "/101589460");
-
-		System.out.println(urlBuilder.toString());
+		String opensourceId = request.getParameter("opensourceId");
+		
+		StringBuilder urlBuilder = new StringBuilder("https://searchcode.com/api/related_results/"+ 92215288 +"/");
+		//urlBuilder.append("92215288" + "/"); // opensourceId 로 바꿔주기!
 
 		URL url = new URL(urlBuilder.toString());
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
 		conn.setRequestProperty("Content-type", "application/json");
 
-		System.out.println("Response code: " + conn.getResponseCode());
+		System.out.println("1) Response code: " + conn.getResponseCode());
 
 		BufferedReader rd;
 
@@ -65,12 +63,77 @@ public class ViewProjectDetailServlet extends HttpServlet {
 			sb.append(line);
 		}
 
-		System.out.println(sb.toString());
 		rd.close();
 		conn.disconnect();
 		
+		String attribute = sb.toString().replace("[", "").replace("]", "");
+		JSONObject jsonObject = new JSONObject(attribute);
+		
+		String reponame = jsonObject.getString("reponame");
+		String source = jsonObject.getString("source");
+		String sourceUrl = jsonObject.getString("sourceurl");
+		int linesCount = jsonObject.getInt("linescount");
+		String location = jsonObject.getString("location");
+		String language = jsonObject.getString("language");
+		String md5hash = jsonObject.getString("md5hash");
+		long id = jsonObject.getLong("id");
+		String fileName = jsonObject.getString("filename");
+		
+		request.setAttribute("repoName", reponame);
+		request.setAttribute("source", source);
+		request.setAttribute("sourceUrl", sourceUrl);
+		request.setAttribute("linesCount", linesCount);
+		request.setAttribute("location", location);
+		request.setAttribute("language", language);
+		request.setAttribute("md5hash", md5hash);
+		request.setAttribute("id", id);
+		request.setAttribute("fileName", fileName);
+		
+		/*
+		 * 파일 트리 파싱
+		 */
+		Document doc = Jsoup.connect(sourceUrl).get();
+		Elements fileTree = doc.select(".file-wrap");
+		
+		request.setAttribute("fileTree", fileTree);
+		
+		/*
+		 * 결과(readme 내용)
+		 */
+		
+		StringBuilder codeUrlBuilder = new StringBuilder("https://searchcode.com/api/result/"+ 92215288 +"/");
+		
+		URL codeUrl = new URL(codeUrlBuilder.toString());
+		conn = (HttpURLConnection) codeUrl.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Content-type", "application/json");
+
+		System.out.println("2) Response code: " + conn.getResponseCode());
+
+		if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+			rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+		} else {
+			rd = new BufferedReader(new InputStreamReader(conn.getErrorStream(), "UTF-8"));
+		}
+		
+		StringBuilder sb2 = new StringBuilder();
+		
+		while ((line = rd.readLine()) != null) {
+			sb2.append(line);
+		}
+		
+		String code = sb2.toString();
+		rd.close();
+		conn.disconnect();
+		
+		JSONObject jsonObject2 = new JSONObject(code);
+		code = jsonObject2.getString("code");
+		
+		request.setAttribute("code", code);
+		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/opensource/detail.jsp");
 		dispatcher.forward(request, response);
+		
 	}
 
 }
--- SourceMeister/src/main/webapp/WEB-INF/view/opensource/detail.jsp
+++ SourceMeister/src/main/webapp/WEB-INF/view/opensource/detail.jsp
@@ -1,20 +1,22 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="Jsoup.java in jsoup located at /src/main/java/org/jsoup">
 <link rel="stylesheet" type="text/css" href="/SourceMeister/static/css/detail_layout.css"/>
-<title>Jsoup.java in jsoup | source code search engine</title>
+<title>${fileName} in ${repoName} | source code search engine</title>
+<script type="text/javascript" src="/SourceMeister/static/js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript">
+	$().ready(function() {
+		$("#fileTree").load();
+	});
+</script>
 </head>
 <body>
 
 	<nav class="navbar navbar-default" role="navigation">
 		<div class="navbar-header">
-			
+			This is a navbar!
 		</div>
 	</nav>
 
@@ -22,82 +24,55 @@
 	<div class="container">
 
 		<div class="search-options">
-			<form action="/">
+			<form action="searchForm">
 				<div class="form-inline">
-					<div class="form-group">
-						<input autocapitalize="off" autocorrect="off" autocomplete="off"
-							spellcheck="true" size="50" placeholder="Search Expression"
-							type="search" class="form-control" name="q" value="">
-					</div>
-					<input type="submit" value="search" class="btn btn-success">
+					<input size="50" placeholder="Search Expression" type="text" name="search" value=""/>
+					<input type="submit" value="search"/>
 				</div>
 			</form>
 		</div>
 		
 		<div class="row">
-			<div id="ad_top"></div>
+			<div class="title">
+				<h4 class="codepath">
+					<a href="${sourceUrl}">${repoName}</a>
+					${location}/${fileName}
+				</h4>
+			</div>
 
-			<h4 class="codepath">
-				<a href="git://github.com/jhy/jsoup.git">jsoup</a>
-				/src/main/java/org/jsoup/Jsoup.java
-			</h4>
+			<div class="header">
+				<table class="table">
+					<tbody>
+						<tr>
+							<th>MD5 Hash</th>
+							<td>${md5hash}</td>
+						</tr>
+						<tr>
+							<th>Repository</th>
+							<td><a href="/${sourceUrl}">${sourceUrl}</a></td>
+						</tr>
+						<!-- <tr>
+							<td colspan="5">
+								<a href="#" id="file-tree-link"> 
+									<span id="file-tree-button" data-id="9911">View File Tree</span>
+								</a>
+							</td>
+						</tr> -->
+					</tbody>
+				</table>
+			</div><br/>
 
-			<table class="table">
-				<tbody>
-					<tr>
-						<td><span class="glyphicon glyphicon-asterisk"
-							aria-hidden="true"></span> Language</td>
-						<td>Java</td>
-						<td><span class="glyphicon glyphicon-wrench"
-							aria-hidden="true"></span> Lines</td>
-						<td>253</td>
-					</tr>
-					<tr>
-						<td><span class="glyphicon glyphicon-globe"
-							aria-hidden="true"></span> Repository</td>
-						<td>git://github.com/jhy/jsoup.git</td>
-					</tr>
-					<tr>
-						<td colspan="5"><a href="#" rel="nofollow"
-							id="view-similar-link" class=""> <span
-								class="glyphicon glyphicon-retweet"></span> <span
-								id="similar-files" data-id="8187896">Show 4 matches</span></a> <a
-							href="#" rel="nofollow" id="file-tree-link" class=""> <span
-								class="glyphicon glyphicon-tree-conifer"></span> <span
-								id="file-tree-button" data-id="9911">View File Tree</span>
-						</a></td>
-					</tr>
-				</tbody>
-			</table>
-
-			<div class="row">
-				<div class="col-md-4" id="file-tree">
-					<div id="file-tree-list"></div>
-				</div>
-				<div class="col-md-8">
-					<table class="highlighttable">
-						<tbody>
-							<tr>
-								<td class="linenos">
-									<div class="linenodiv">
-										<pre>
-	      								</pre>
-									</div>
-								</td>
-								<td class="code">
-									<div class="highlight">
-										<pre>
-										</pre>
-									</div>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
+			<div class="body">
+				<textarea readonly="readonly" style="resize: none; wrap:hard;" cols="110" rows="20">${code}</textarea>
+			</div><br/>
+			
+			<div class="fileTree">
+				${fileTree}
 			</div>
 
 			<div class="footer">
 			</div>
+			
 		</div>
 
 	</div>
